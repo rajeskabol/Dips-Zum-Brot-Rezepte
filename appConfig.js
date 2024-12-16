ebaa29a39049971b84f6382f5aef0df6a4ff862a
rajeskabol <rajeskabol@gmail.com>
2024-12-17 06:43:17 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   title: "JUDUL",
-  tagline: "Coloring Me Free Coloring Page",
-  description: "Coloring Me Free Coloring Pag.",
-  keywords: "coloring,coloringkids,agc,cdom blog",
-  author: "Coloringme.biz.id",
-  favicon: "https://pugjs.org/images/favicon.ico",
+  tagline: "Dein ultimativer Leitfaden für köstliche Dips zum Brot! Entdecke eine Vielfalt an herzhaften und süßen Saucen, perfekt für jede Gelegenheit.",
+  description: "Tauche ein in die Welt der Dips! Hier findest du einfache und leckere Rezepte für deine nächste Brotzeit oder Party.",
+  keywords: "Leckere Dips zu Baguette,Dips zum Selbermachen,Party Dips für Brot,dips, die beliebtesten rezepte,Schnelle Dips für Brot,Dip Rezepte für Gemüsesticks,Dips selber machen mit Frischkäse,Dips zum Grillen",
+  author: "Milyader Muda",
+  favicon: "",
   imageCount: 20,
   removeBadWords: "porn,nude,sex",
 };
