70724e9995b95d8e2e3352a6c5d8b8ba94b3b35f
rajeskabol <rajeskabol@gmail.com>
2024-12-17 06:43:32 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "JUDUL",
+  title: "Dips Zum Brot Rezepte",
   tagline: "Dein ultimativer Leitfaden für köstliche Dips zum Brot! Entdecke eine Vielfalt an herzhaften und süßen Saucen, perfekt für jede Gelegenheit.",
   description: "Tauche ein in die Welt der Dips! Hier findest du einfache und leckere Rezepte für deine nächste Brotzeit oder Party.",
   keywords: "Leckere Dips zu Baguette,Dips zum Selbermachen,Party Dips für Brot,dips, die beliebtesten rezepte,Schnelle Dips für Brot,Dip Rezepte für Gemüsesticks,Dips selber machen mit Frischkäse,Dips zum Grillen",
